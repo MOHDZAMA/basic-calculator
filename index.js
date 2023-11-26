9062f70731654273f14282e9a07f5bf96f5f551a
MOHDZAMA <106185773+MOHDZAMA@users.noreply.github.com>
2023-11-26 13:57:32 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".buttons");
-const specialChars = ["%", "*", "/", "-", "+", "="];
+const buttons = document.querySelector(".buttons");
+// const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
 buttons.addEventListener("click", (e) => {
@@ -9,24 +9,9 @@ buttons.addEventListener("click", (e) => {
   } else if (e.target.value == "DEL") {
     let val = display.value;
     display.value = val.substring(0, val.length - 1);
+  } else if (e.target.value == "=") {
+    display.value = eval(display.value);
   } else {
     display.value += e.target.value;
   }
 });
-//Define function to calculate based on button clicked.
-const calculate = (btnValue) => {
-  display.focus();
-  if (btnValue === "=" && output !== "") {
-    //If output has '%', replace with '/100' before evaluating.
-    output = eval(output.replace("%", "/100"));
-  } else if (btnValue === "AC") {
-    output = "";
-  } else if (btnValue === "DEL") {
-    //If DEL button is clicked, remove the last character from the output.
-    output = output.toString().slice(0, -1);
-  } else {
-    //If output is empty and button is specialChars then return
-    if (output === "" && specialChars.includes(btnValue)) return;
-    output += btnValue;
-  }
-};
